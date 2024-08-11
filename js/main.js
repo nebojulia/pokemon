1b22be7cd7284ef50095aac14f5706cd1457ee44
Julia Nebo <80979975+nebojulia@users.noreply.github.com>
2024-08-11 15:52:46 +0300

    add js for mobile filter block

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("filter").addEventListener("click", function() {
+        document.getElementById("container").classList.toggle("open")
+    })
+})
