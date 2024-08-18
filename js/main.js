eea5a21d3ba14d57cff7926e7ad5e5970b309872
Julia Nebo <80979975+nebojulia@users.noreply.github.com>
2024-08-18 21:08:19 +0300

    add burger btn + filter open animation

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("burger").addEventListener("click", function() {
+        document.getElementById("tabs").classList.toggle("open")
+    })
+})
