c4652a6cc2d99ae4b821b2dd35613d22aaff92ef
Julia Nebo <80979975+nebojulia@users.noreply.github.com>
2024-08-18 23:35:23 +0300

    fix btn

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
+//let filterButton = document.querySelectorAll("filter__button");
+
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("burger").addEventListener("click", function() {
         document.getElementById("tabs").classList.toggle("open")
     })
+    // document.filterButton.addEventListener("click", function() {
+    //     document.getElementById("tabs").classList.toggle("open")
+    // })
 })
