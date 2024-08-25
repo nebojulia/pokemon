153fa3df24e1381432e7f0ed3ced95ed12e58d71
Julia Nebo <80979975+nebojulia@users.noreply.github.com>
2024-08-25 20:03:43 +0300

    fix styles

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
-//let filterButton = document.querySelectorAll("filter__button");
-
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("burger").addEventListener("click", function() {
-        document.getElementById("tabs").classList.toggle("open")
+    document.querySelector(".burger").addEventListener("click", function() {
+        document.querySelector(".tabs").classList.toggle("open")
     })
-    // document.filterButton.addEventListener("click", function() {
-    //     document.getElementById("tabs").classList.toggle("open")
-    // })
 })
